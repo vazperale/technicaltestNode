ed754d15bbd014543ae11bfe71961f4d02e86375
vazperale <alexvazquezperezvp@gmail.com>
2025-05-08 20:33:08 +0200

    add swagger json for documentation and cors for connection with web client

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,24 @@ import express from 'express';
 import { connectDB } from './config/typeorm.config';
 import organizationRoutes from './routes/organization.routes';
 import chargePointRoutes from './routes/chargepoint.routes';
+import cors from 'cors';
+const swaggerDocument = require("./swagger.json");
+import swaggerUi from 'swagger-ui-express';
+
 
 const app = express();
 const port = 3000;
 
+app.use(cors({
+  origin: '*',
+}));
+
 app.use(express.json());
 
+
 connectDB();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/',organizationRoutes);
 app.use('/', chargePointRoutes);
